857c977198d903698069773f2096ac3259a8bd54
wheavin <william.james.heavin@gmail.com>
2021-12-29 21:28:50 +0000

    System property to enable data update on startup

--- f1-stats-data/src/main/java/com/william/dev/f1stats/data/remote/StartupBean.java
+++ f1-stats-data/src/main/java/com/william/dev/f1stats/data/remote/StartupBean.java
@@ -18,7 +18,21 @@ public class StartupBean {
     @PostConstruct
     private void init() {
         log.info("Application has started");
-        dataUpdaterBean.updateCircuits();
-        dataUpdaterBean.updateDrivers();
+        if (isUpdateOnStartup()) {
+            log.info("Updating data...");
+            dataUpdaterBean.updateCircuits();
+            dataUpdaterBean.updateDrivers();
+        } else {
+            log.info("Skipping data update");
+        }
+    }
+
+    private boolean isUpdateOnStartup() {
+        try {
+            return Boolean.parseBoolean(System.getProperty("f1stats.updateonstartup"));
+        } catch (final Exception ex) {
+            log.info("System property not set", ex);
+            return false;
+        }
     }
 }
